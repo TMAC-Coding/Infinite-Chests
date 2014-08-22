997e88a85a4322fba0baf9e3545371d08446b278
TMAC_Kratos <troyrash001@gmail.com>
2014-08-22 13:01:43 -0700

    Making scripts easier to write
    Making scripts easier to write for CI

---
+++ prepare.sh
@@ -0,0 +1,7 @@
+sudo start xvfb
+wget https://dl.dropboxusercontent.com/u/54314339/bukkit/craftbukkit.jar --quiet
+wget https://dl.dropboxusercontent.com/u/54314339/bukkit/bukkit.yml --quiet
+wget https://dl.dropboxusercontent.com/u/54314339/bukkit/banned-ips.json --quiet
+wget https://dl.dropboxusercontent.com/u/54314339/bukkit/banned-players.json --quiet
+wget https://dl.dropboxusercontent.com/u/54314339/bukkit/ops.json --quiet
+wget https://dl.dropboxusercontent.com/u/54314339/bukkit/whitelist.json --quiet
