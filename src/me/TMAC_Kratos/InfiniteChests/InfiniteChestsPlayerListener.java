9bbe08ad171416d3532a66c56081f453a6c0a59a
TMAC-Kratos <kratos@tmachq.com>
2014-08-22 01:48:02 -0700

    Update InfiniteChestsPlayerListener.java
    forgot to import

--- src/me/TMAC_Kratos/InfiniteChests/InfiniteChestsPlayerListener.java
+++ src/me/TMAC_Kratos/InfiniteChests/InfiniteChestsPlayerListener.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
