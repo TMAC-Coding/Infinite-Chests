1370d07666bc88ef03f8712c469e86688d996f39
TMAC_Kratos <troyrash001@gmail.com>
2014-08-22 13:43:07 -0700

    Updated prepare.sh
    More fixes for directory create and printing lines.

--- prepare.sh
+++ prepare.sh
@@ -1,24 +1,24 @@
 sudo start xvfb
-print "Receiving all Files..."
+printf "Receiving all Files..."
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/craftbukkit.jar --quiet
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/bukkit.yml --quiet
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/banned-ips.json --quiet
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/banned-players.json --quiet
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/ops.json --quiet
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/whitelist.json --quiet
-mkdir dir="world"
+mkdir world
 cd world
-mkdir dir="region"
+mkdir region
 cd region
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/world/region/r.0.0.mca --quiet
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/world/region/r.-1.0.mca --quiet
 cd ..
 cd ..
-mkdir dir="world_nether"
+mkdir world_nether
 cd world_nether
-mkdir dir="DIM-1"
+mkdir DIM-1
 cd DIM-1
-mkdir dir="region"
+mkdir region
 cd region
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/world_nether/DIM-1/region/r.0.0.mca --quiet
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/world_nether/DIM-1/region/r.0.-1.mca --quiet
@@ -27,11 +27,11 @@ wget https://dl.dropboxusercontent.com/u/54314339/bukkit/world_nether/DIM-1/regi
 cd ..
 cd ..
 cd ..
-mkdir dir="world_the_end"
+mkdir world_the_end
 cd world_the_end
-mkdir dir="DIM-1"
+mkdir DIM-1
 cd DIM-1
-mkdir dir="region"
+mkdir region
 cd region
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/world_the_end/DIM1/region/r.0.0.mca --quiet
 wget https://dl.dropboxusercontent.com/u/54314339/bukkit/world_the_end/DIM1/region/r.0.-1.mca --quiet
@@ -40,4 +40,4 @@ wget https://dl.dropboxusercontent.com/u/54314339/bukkit/world_the_end/DIM1/regi
 cd ..
 cd ..
 cd ..
-print "Finished Receiving."
+printf "Finished Receiving."
